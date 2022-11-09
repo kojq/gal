771b4cc93a7e3eb3d91ea4db38954653af56a606
kojq <106934605+kojq@users.noreply.github.com>
2022-11-09 00:47:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 cd ~
 
 echo 'SUBSYSTEM=="block", ENV{ID_FS_TYPE}=="ntfs", ENV{ID_FS_TYPE}="ntfs3"' | sudo tee /etc/udev/rules.d/ntfs3_by_default.rules
-sudo sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 36/" /etc/pacman.conf
+sudo sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 24/" /etc/pacman.conf
 
 #tools
-sudo pacman -Syu git wget curl fzf helix imagemagick ffmpeg mpv bluez bluez-utils
+sudo pacman -Syu git wget curl fzf helix imagemagick ffmpeg mpv bluez bluez-utils fwupd
 
 #applications
 sudo pacman -S lapce qbittorrent blender inkscape gimp obs-studio #diffuse meld desmume dolphin-emu
@@ -28,13 +28,13 @@ rm -rf paru-bin
 paru -S fastfetch cloudflare-warp-bin ventoy-bin #pandoc-bin linux-wifi-hotspot
 
 #applications
-paru -S brave-bin onlyoffice-bin bottles #cemu citra-qt-bin yuzu-mainline-bin ryujinx-git visual-studio-code-bin jetbrains-fleet
+paru -S brave-bin onlyoffice-bin #bottles cemu citra-qt-bin yuzu-mainline-bin ryujinx-git visual-studio-code-bin jetbrains-fleet
 
 #fonts
 #paru -S apple-fonts ttf-ms-win11-auto
 
 #asus tools
-paru -S asusctl
+paru -S asusctl supergfxctl-git
 
 systemctl enable --now nftables
 systemctl enable --now bluetooth
